491937862d7b3bea967d894b3c20061feb18d809
lucaszw <lucas.zeer@gmail.com>
2018-05-14 19:33:37 -0400

    add ui for x & y

--- packages/medella-bot/src/medella-bot.js
+++ packages/medella-bot/src/medella-bot.js
@@ -4,9 +4,25 @@ const UIPlugin = require('@scicad/ui-plugin');
 class MedellaBot extends UIPlugin {
   constructor(elem, focusTracker, port, ...args) {
     super(elem, focusTracker, port, ...args);
-    this.element.appendChild(yo`
-      <b>Hello World!</b>
-    `);
+
+    let moveContainer;
+    moveContainer = yo`
+      <div>
+        <b>Move To: </b>
+        <p>x : <input type="number" name="x" min="0" value="0"></p>
+        <p>y : <input type="number" name="y" min="0" value="0"></p>
+        <button type="submit" onclick=${()=>this.moveTo(moveContainer)}>
+          Move
+        </button>
+      </div>
+    `;
+    this.element.appendChild(moveContainer);
+  }
+
+  moveTo(container){
+    let x = parseFloat(container.querySelector("[name='x']").value);
+    let y = parseFloat(container.querySelector("[name='y']").value);
+    // TODO: Update yac to support node plugins
   }
 }
 
