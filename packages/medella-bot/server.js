0d89e6447dfdf8099fc24fd866cc1087cee29fbb
lucaszw <lucas.zeer@gmail.com>
2018-05-15 19:01:24 -0400

    move gerber controls to yac plugin

--- packages/medella-bot/server.js
+++ packages/medella-bot/server.js
@@ -1,4 +1,43 @@
-console.log("Hello World!");
-setInterval(()=> {
-  console.log("Hi")
-}, 500);
+const os = require('os');
+const path = require('path');
+
+const SerialPort = require('serialport');
+
+const {MicropedeClient} = require('@micropede/client/src/client.js');
+const APPNAME = 'scicad';
+const PORT = 1884;
+
+var base = new SerialPort('COM12', {
+  baudRate: 115200
+});
+
+class MedellaBotServer extends MicropedeClient {
+  constructor(...args) {
+    super(...args);
+  }
+  listen() {
+    this.onTriggerMsg('move', this.move.bind(this));
+    base.write('X0 Y0 Z0\n');
+  }
+  move(payload){
+    let {axis, value} = payload;
+    if (axis == 'x') {
+      base.write(`X${value}\n`);
+    } else if (axis == 'y') {
+      base.write(`Y${value} Z${value}\n`);
+    }
+  }
+}
+
+base.on('open', function(){
+  let initialized = false;
+  console.log('Serial Port Opened');
+  base.on('data', function (data) {
+    if (initialized == false) {
+      const bot = new MedellaBotServer(APPNAME, undefined, PORT);
+    }
+    const message = data.toString().replace('\r\n', '');
+    console.log({message});
+    initialized = true;
+  });
+});
--- packages/medella-bot/src/medella-bot.js
+++ packages/medella-bot/src/medella-bot.js
@@ -9,20 +9,30 @@ class MedellaBot extends UIPlugin {
     moveContainer = yo`
       <div>
         <b>Move To: </b>
-        <p>x : <input type="number" name="x" min="0" value="0"></p>
-        <p>y : <input type="number" name="y" min="0" value="0"></p>
-        <button type="submit" onclick=${()=>this.moveTo(moveContainer)}>
-          Move
-        </button>
+        <p>x :
+          <input type="number" name="x" min="0" value="0">
+          <button onclick=${()=>this.moveTo(moveContainer, "x")}>Move</button>
+        </p>
+        <p>y :
+          <input type="number" name="y" min="0" value="0">
+          <button onclick=${()=>this.moveTo(moveContainer, "y")}>Move</button>
+        </p>
       </div>
     `;
     this.element.appendChild(moveContainer);
   }
+  listen() {
+    this.bindTriggerMsg('medella-bot-server', 'move', 'move');
+  }
 
-  moveTo(container){
+  moveTo(container, axis){
+    let value;
     let x = parseFloat(container.querySelector("[name='x']").value);
     let y = parseFloat(container.querySelector("[name='y']").value);
-    // TODO: Update yac to support node plugins
+    if (axis == 'x') value = x;
+    if (axis == 'y') value = y;
+
+    this.trigger('move', {axis, value});
   }
 }
 
