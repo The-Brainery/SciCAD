42bcddbf461ad686aa0f8ecb48c928106650ab05
lucaszw <lucas.zeer@gmail.com>
2018-05-24 12:56:55 -0400

    move gui into parent container

--- packages/device-ui-plugin-2/src/device-ui-plugin-2.js
+++ packages/device-ui-plugin-2/src/device-ui-plugin-2.js
@@ -6,46 +6,6 @@ const dat = require('dat.gui');
 const yo = require('yo-yo');
 const _ = require('lodash');
 
-const getScaledCoordinates = (origCorners, bbox, scale) => {
-  const corners = [];
-
-  for (var i = 0; i != 8; i += 2) {
-    const x_tl = origCorners[i];
-    const y_tl = origCorners[i+1];
-    const x_c = bbox.width/2 - x_tl;
-    const y_c = bbox.height/2 - y_tl;
-
-    const x_c_scaled = x_c * scale;
-    const y_c_scaled = y_c * scale;
-
-
-    const x_tl_scaled = bbox.width/2 - x_c_scaled;
-    const y_tl_scaled = bbox.height/2 - y_c_scaled;
-    corners[i] = x_tl_scaled;
-    corners[i+1] = y_tl_scaled;
-  }
-
-  return corners;
-}
-
-const getOriginalCoordinates = (x, y, bbox, scale) => {
-
-  const x_tl_scaled = x;
-  const y_tl_scaled = y;
-
-  const x_c_scaled = bbox.width/2 - x_tl_scaled;
-  const y_c_scaled = bbox.height/2 - y_tl_scaled;
-
-  const x_c = x_c_scaled/scale;
-  const y_c = y_c_scaled/scale;
-
-  const x_tl = bbox.width/2 - x_c;
-  const y_tl = bbox.height/2 - y_c;
-
-  return {x: x_tl, y: y_tl};
-}
-
-
 class Device2UIPlugin extends UIPlugin {
   constructor(elem, focusTracker, port, ...args) {
     super(elem, focusTracker, port, ...args);
@@ -54,29 +14,9 @@ class Device2UIPlugin extends UIPlugin {
     this.shiftDown = false;
     this.prevAnchors = [];
     this.scale = 1;
-    this.createScene("top");
-
-    // Construct menu
-    let _this = this;
-    var menu = {
-      get flipForeground() {
-        return this._flipForeground || false;
-      },
-      set flipForeground(_flipForeground) {
-        if (_flipForeground == true) {
-          _this.createScene('bottom');
-          _this.initTransform();
-        }
-        if (_flipForeground == false) {
-          _this.createScene('top');
-          _this.initTransform();
-        }
-        this._flipForeground = _flipForeground;
-      }
-    };
-
-    var gui = new dat.GUI();
-    gui.add(menu, 'flipForeground');
+    this.svgControls = CreateScene(this);
+    this.gui = CreateGUI(this);
+    this.element.appendChild(this.gui.domElement);
   }
 
   listen() {
@@ -95,19 +35,23 @@ class Device2UIPlugin extends UIPlugin {
       if (this.hasFocus == true) this.svgControls.moveLocal(e);
     });
 
-    // TODO: Replace setTImeout with some page ready event...
     setTimeout(()=> {
       this.initTransform();
       this.element.addEventListener("mousedown", this.mousedown.bind(this));
       this.element.addEventListener("mouseup", this.mouseup.bind(this));
       this.element.addEventListener("mousemove", this.move.bind(this));
+      let markers = this.element.querySelectorAll(".corner");
 
       document.addEventListener("keydown", (e) => {
-        if (e.key == "Shift") this.shiftDown = true;
+        if (e.key != "Shift") return;
+        this.shiftDown = true;
+        _.each(markers, (m) => m.style.background = "green");
       });
 
       document.addEventListener("keyup", (e) => {
-        if (e.key == "Shift") this.shiftDown = false;
+        if (e.key != "Shift") return;
+        this.shiftDown = false;
+        _.each(markers, (m) => m.style.background = "white");
       });
 
     }, 1000);
@@ -115,7 +59,6 @@ class Device2UIPlugin extends UIPlugin {
   }
 
   update() {
-    let box = this.box;
     let markers = this.element.querySelectorAll(".corner");
     for (var i = 0; i != 8; i += 2) {
       var marker = document.getElementById(`marker${i}`);
@@ -157,7 +100,7 @@ class Device2UIPlugin extends UIPlugin {
         this.currentcorner = i;
       }
     }
-    // localStorage.setItem("corners", JSON.stringify(this.corners));
+
     this.move(e);
   }
 
@@ -184,7 +127,8 @@ class Device2UIPlugin extends UIPlugin {
   }
 
   transform2d(...points) {
-    let w = parseFloat(this.box.style.width), h = parseFloat(this.box.style.height);
+    let box = this.element.querySelector("#box");
+    let w = parseFloat(box.style.width), h = parseFloat(box.style.height);
     let transform, t;
 
     if (this.prevAnchors.length <= 0)
@@ -202,70 +146,13 @@ class Device2UIPlugin extends UIPlugin {
     localStorage.setItem("prevTransform", JSON.stringify(this.prevTransform))
 
     t = "matrix3d(" + t.join(", ") + ")";
-    this.box.style["-webkit-transform"] = t;
-    this.box.style["-moz-transform"] = t;
-    this.box.style["-o-transform"] = t;
-    this.box.style.transform = t;
-  }
-  createScene(placement) {
-    let background, foreground, deviceContainer, video;
-    if (placement == 'top') {
-      background = deviceContainer = yo`
-      <div style="opacity: 0.5;z-index:10;${Styles.background}">
-      </div> `;
-
-      foreground = video = yo`
-        <video id="video" style="z-index:5;position:relative;object-fit: fill; width:100%;height:100%;" autoplay></video>
-      `;
-    } else {
-      background = video = yo`
-        <video id="video" style="object-fit: fill;${Styles.background}" autoplay></video>
-      `;
-
-      foreground = deviceContainer = yo`
-        <div style="opacity: 0.5;"></div>
-      `;
-    }
-
-    this.box = yo`
-      <div id="box" style="${Styles.box}">
-        ${foreground}
-      </div>
-    `;
-
-    this.background = background;
-
-    let container = yo`
-      <div id="container-outer" style="${Styles.container}">
-        <div id="container" style="${Styles.container}">
-            ${background}
-            ${this.box}
-            <div id="marker0" style="${Styles.corner}"class="corner">TL</div>
-            <div id="marker2" style="${Styles.corner}"class="corner">TR</div>
-            <div id="marker4" style="${Styles.corner}"class="corner">BL</div>
-            <div id="marker6" style="${Styles.corner}"class="corner">BR</div>
-        </div>
-      </div>
-    `;
-
-    this.element.innerHTML = '';
-    this.element.appendChild(container);
-
-    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-          video.src = window.URL.createObjectURL(stream);
-          video.play();
-      });
-    }
-    this.svgControls = new SvgControls(deviceContainer);
-
+    box.style["-webkit-transform"] = t;
+    box.style["-moz-transform"] = t;
+    box.style["-o-transform"] = t;
+    box.style.transform = t;
   }
 
   initTransform() {
-
-    // if (localStorage.getItem("prevTransform") != null) {
-    //   this.prevTransform = JSON.parse(localStorage.getItem("prevTransform"));
-    // }
     if (localStorage.getItem("prevAnchors") != null) {
       this.prevAnchors = JSON.parse(localStorage.getItem("prevAnchors"));
     }
@@ -273,7 +160,6 @@ class Device2UIPlugin extends UIPlugin {
       this.corners = JSON.parse(localStorage.getItem("corners"));
     }
 
-    let box = this.box;
     let markers = this.element.querySelectorAll(".corner");
     let corners = this.corners;
     this.transform2d(...this.corners);
@@ -337,5 +223,124 @@ const Styles = {
     transform: scale(1);
   `
 }
+
+
+const getScaledCoordinates = (origCorners, bbox, scale) => {
+  const corners = [];
+
+  for (var i = 0; i != 8; i += 2) {
+    const x_tl = origCorners[i];
+    const y_tl = origCorners[i+1];
+    const x_c = bbox.width/2 - x_tl;
+    const y_c = bbox.height/2 - y_tl;
+
+    const x_c_scaled = x_c * scale;
+    const y_c_scaled = y_c * scale;
+
+
+    const x_tl_scaled = bbox.width/2 - x_c_scaled;
+    const y_tl_scaled = bbox.height/2 - y_c_scaled;
+    corners[i] = x_tl_scaled;
+    corners[i+1] = y_tl_scaled;
+  }
+
+  return corners;
+}
+
+const getOriginalCoordinates = (x, y, bbox, scale) => {
+
+  const x_tl_scaled = x;
+  const y_tl_scaled = y;
+
+  const x_c_scaled = bbox.width/2 - x_tl_scaled;
+  const y_c_scaled = bbox.height/2 - y_tl_scaled;
+
+  const x_c = x_c_scaled/scale;
+  const y_c = y_c_scaled/scale;
+
+  const x_tl = bbox.width/2 - x_c;
+  const y_tl = bbox.height/2 - y_c;
+
+  return {x: x_tl, y: y_tl};
+}
+
+const CreateGUI = (deviceUIPlugin) => {
+  let gui;
+
+  var menu = {
+    get flipForeground() {
+      return this._flipForeground || false;
+    },
+    set flipForeground(_flipForeground) {
+      if (_flipForeground == true) {
+        CreateScene(deviceUIPlugin, 'bottom');
+        deviceUIPlugin.initTransform();
+        deviceUIPlugin.element.appendChild(gui.domElement);
+      }
+      if (_flipForeground == false) {
+        CreateScene(deviceUIPlugin, 'top');
+        deviceUIPlugin.initTransform();
+        deviceUIPlugin.element.appendChild(gui.domElement);
+      }
+      this._flipForeground = _flipForeground;
+    }
+  };
+
+  gui = new dat.GUI({autoPlace: false});
+  gui.add(menu, 'flipForeground');
+  gui.domElement.style.position = "absolute";
+  gui.domElement.style.top = "0px";
+  gui.domElement.style.height = "100%";
+
+  return gui;
+}
+
+const CreateScene = (deviceUIPlugin, placement='top') => {
+  let background, foreground, deviceContainer, video;
+  if (placement == 'top') {
+    background = deviceContainer = yo`
+    <div style="opacity: 0.5;z-index:10;${Styles.background}">
+    </div> `;
+
+    foreground = video = yo`
+      <video id="video" style="z-index:5;position:relative;object-fit: fill; width:100%;height:100%;" autoplay></video>
+    `;
+  } else {
+    background = video = yo`
+      <video id="video" style="object-fit: fill;${Styles.background}" autoplay></video>
+    `;
+
+    foreground = deviceContainer = yo`
+      <div style="opacity: 0.5;"></div>
+    `;
+  }
+
+  let container = yo`
+    <div id="container-outer" style="${Styles.container}">
+      <div id="container" style="${Styles.container}">
+          ${background}
+          <div id="box" style="${Styles.box}">
+            ${foreground}
+          </div>
+          <div id="marker0" style="${Styles.corner}"class="corner">TL</div>
+          <div id="marker2" style="${Styles.corner}"class="corner">TR</div>
+          <div id="marker4" style="${Styles.corner}"class="corner">BL</div>
+          <div id="marker6" style="${Styles.corner}"class="corner">BR</div>
+      </div>
+    </div>
+  `;
+
+  deviceUIPlugin.element.innerHTML = '';
+  deviceUIPlugin.element.appendChild(container);
+
+  if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+    });
+  }
+  return new SvgControls(deviceContainer);
+}
+
 
 module.exports = Device2UIPlugin;
